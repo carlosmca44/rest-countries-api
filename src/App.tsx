34bc2f7e891f8c8718406ab4be9c611bbb2416f5
carlosmca44 <carlosmca4424@gmail.com>
2022-06-26 17:44:09 -0400

    lazy on routes implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Countries from "./components/Countries";
-import CountryDescription from "./components/CountryDescription";
+
+const Countries = React.lazy(() => import("./components/Countries"));
+const CountryDescription = React.lazy(() => import("./components/CountryDescription"));
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Countries />} />
-        <Route path="/description" element={<CountryDescription />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Countries />} />
+          <Route path="/description" element={<CountryDescription />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
