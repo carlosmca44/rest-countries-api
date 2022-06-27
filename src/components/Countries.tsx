b574e9c0018a93333ca6d4f79c1a7e0c6fff91d1
carlosmca44 <carlosmca4424@gmail.com>
2022-06-27 13:21:15 -0400

    countries structure styles

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import useFetch from "../hooks/useFetch";
+import Card from "./Card";
 
 const Countries: React.FC = () => {
-  return <>countries</>;
+  const { data, loading, error } = useFetch('https://restcountries.com/v3.1/all')
+
+  //console.log(data[2])
+
+  if (loading) {
+    return (
+      <p>Loading text</p>
+    )
+  }
+
+  if (error) {
+    return (
+      <p>ERROR</p>
+    )
+  }
+
+  return (
+    <div className="countries-root-container">
+      <div className="grid-container">
+        {data.map((item: any, key: any) => {
+          return (
+              <Card 
+                key={key}
+                img={item.flags.png}
+                name={item.name.official}
+                population={item.population}
+                region={item.region}
+                capital={item.capital}
+              />
+          )
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
