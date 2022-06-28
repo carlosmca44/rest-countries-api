36dd04ec6e7fd0a252f9e107afd067f4683f9819
carlosmca44 <carlosmca4424@gmail.com>
2022-06-28 11:40:15 -0400

    searcher implemented

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -1,38 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useFetch from "../hooks/useFetch";
 import Card from "./Card";
+import Filter from "./Filter";
+import Searcher from "./Searcher";
 
 const Countries: React.FC = () => {
-  const { data, loading, error } = useFetch('https://restcountries.com/v3.1/all')
+  const { data, loading, error } = useFetch(
+    "https://restcountries.com/v3.1/all"
+  );
 
-  //console.log(data[2])
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const searcher = (e: any) => {
+    setSearch(e.target.value);
+  };
+
+  let results = [];
+  if (!search) {
+    results = countries;
+  } else {
+    results = countries.filter((data) =>
+      data["name"]["common"]
+        .toLocaleLowerCase()
+        .includes(search.toLocaleLowerCase())
+    );
+  }
+
+  useEffect(() => {
+    setCountries(data);
+  }, [data]);
 
   if (loading) {
-    return (
-      <p>Loading text</p>
-    )
+    return <p>Loading text</p>;
   }
 
   if (error) {
-    return (
-      <p>ERROR</p>
-    )
+    return <p>ERROR</p>;
   }
 
   return (
     <div className="countries-root-container">
+      <div className="filters-container">
+        <Searcher value={search} onChange={searcher} />
+        <Filter />
+      </div>
       <div className="grid-container">
-        {data.map((item: any, key: any) => {
+        {results.map((item: any, key: any) => {
           return (
-              <Card 
-                key={key}
-                img={item.flags.png}
-                name={item.name.official}
-                population={item.population}
-                region={item.region}
-                capital={item.capital}
-              />
-          )
+            <Card
+              key={key}
+              img={item.flags.png}
+              name={item.name.common}
+              population={item.population}
+              region={item.region}
+              capital={item.capital}
+            />
+          );
         })}
       </div>
     </div>
---
+++ src/components/Searcher.tsx
@@ -0,0 +1,23 @@
+import React, { ChangeEventHandler } from "react";
+import SearchIcon from "./svg/SearchIcon";
+
+type SearcherProps = {
+  value?: string;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+};
+
+const Searcher: React.FC<SearcherProps> = ({ value, onChange }) => {
+  return (
+    <div className="searcher-container">
+      <SearchIcon />
+      <input
+        value={value}
+        onChange={onChange}
+        placeholder="Search for a country..."
+        type="text"
+      />
+    </div>
+  );
+};
+
+export default Searcher;
